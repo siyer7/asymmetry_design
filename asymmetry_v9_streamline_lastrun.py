b8623ee6dafb7750ebf8fede16e039d33e4aa93e
Siddhant Iyer <siddhant.kumar.iyer@gmail.com>
2025-03-02 21:24:56 -0500

    final update for sunday night

--- asymmetry_v9_streamline_lastrun.py
+++ asymmetry_v9_streamline_lastrun.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 This experiment was created using PsychoPy3 Experiment Builder (v2024.2.4),
-    on Sun Mar  2 21:05:43 2025
+    on Sun Mar  2 21:22:31 2025
 If you publish work using this script the most relevant publication is:
 
     Peirce J, Gray JR, Simpson S, MacAskill M, Höchenberger R, Sogo H, Kastman E, Lindeløv JK. (2019) 

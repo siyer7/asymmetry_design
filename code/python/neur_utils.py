4bc05fdc19a86d164ee69cc28e509ee8242d1f74
siyer7 <siddhant.kumar.iyer@gmail.com>
2026-05-27 11:44:57 -0400

    reorganized directory

--- code/python/neur_utils.py
+++ code/python/neur_utils.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 
 def get_pt_metadata(psychopy_df, patient, verbose=True):
-    pt_data_dir = f'../../data/2025{int(patient)}/records/processed_data'
+    pt_data_dir = f'../../outputs/processed_data/2025{int(patient)}'
     pt_psychopy_df = psychopy_df.loc[psychopy_df['subj'] == patient].reset_index(drop=True)
     pt_neur_df = pd.read_parquet(f'{pt_data_dir}/df_neurs.parquet')
     pt_num_neurs = len(pt_neur_df)
@@ -13,12 +13,12 @@ def get_pt_metadata(psychopy_df, patient, verbose=True):
 
 
 def get_pt_epoch_spike_data(patient, epoch, verbose=True):
-    pt_data_dir = f'../../data/2025{int(patient)}/records/processed_data'
-    pt_epoch_spikes = np.load(f'{pt_data_dir}/{epoch}_spikes.npy', allow_pickle=True)
+    pt_matrices_dir = f'../../outputs/processed_data/2025{int(patient)}/matrices'
+    pt_epoch_spikes = np.load(f'{pt_matrices_dir}/{epoch}_spikes.npy', allow_pickle=True)
     pt_epoch_num_spikes = np.array([[len(pt_epoch_spikes[t, n]) for n in range(pt_epoch_spikes.shape[1])]
                                     for t in range(pt_epoch_spikes.shape[0])])
-    pt_epoch_FRs = np.load(f'{pt_data_dir}/{epoch}_FRs.npy', allow_pickle=True)
-    pt_epoch_bins = np.load(f'{pt_data_dir}/{epoch}_bin_centers.npy', allow_pickle=True)
+    pt_epoch_FRs = np.load(f'{pt_matrices_dir}/{epoch}_FRs.npy', allow_pickle=True)
+    pt_epoch_bins = np.load(f'{pt_matrices_dir}/{epoch}_bin_centers.npy', allow_pickle=True)
     if verbose:
         print(f'patient={patient}, epoch={epoch}')
         print(f'spikes (trials, neurons): {pt_epoch_spikes.shape}')

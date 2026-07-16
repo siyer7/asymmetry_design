e420a495af67e4c0eeb67a80585bfd763e3ce126
siyer7 <siddhant.kumar.iyer@gmail.com>
2026-07-16 12:57:56 -0400

    updated network to one head only!

---
+++ code/python/utils.py
@@ -0,0 +1,17 @@
+def plot_style(context):
+    import seaborn as sns, matplotlib.pyplot as plt, matplotlib as mpl
+
+    # sets fontsize etc. appropriate for presentation/paper, etc.
+    sns.set(context=context, style='ticks', palette='deep')
+
+    # keep text editable in svg
+    plt.rcParams['svg.fonttype'] = 'none'
+
+    # push ticks inward
+    mpl.rcParams['xtick.direction'], mpl.rcParams['ytick.direction'] = 'in', 'in'
+    # remove top and right splines
+    mpl.rcParams['axes.spines.top'], mpl.rcParams['axes.spines.right'] = False, False
+
+def norm01(x):
+    import numpy as np
+    return (x - np.nanmin(x)) / (np.nanmax(x) - np.nanmin(x))
